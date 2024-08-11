6a189d767eecf32dba9252c0652752eecef446d6
hongyuqin <hongyuqin@github.com>
2024-08-11 09:21:39 +0800

    0811 prometheus

--- src/main/java/com/huf/g1test/G1testApplication.java
+++ src/main/java/com/huf/g1test/G1testApplication.java
@@ -4,16 +4,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan("com.huf.g1test.mapper")
 @Slf4j
 public class G1testApplication {
 
-
-
-		public static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(G1testApplication.class, args);
-		}
+	}
+
+
+
 
 }
