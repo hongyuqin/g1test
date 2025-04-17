ff1ad6bce2cf951fe6ecad5ceb4b613cec1e3522
hongyuqin <896664230@qq.com>
2025-04-17 10:10:49 +0800

    redis

--- src/main/java/com/huf/g1test/controller/HelloController.java
+++ src/main/java/com/huf/g1test/controller/HelloController.java
@@ -1,17 +1,32 @@
 package com.huf.g1test.controller;
 
 import com.huf.g1test.service.CService;
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class HelloController {
     @Autowired
     private CService cService;
+    @Autowired
+    private Redisson redisson;
 
     @GetMapping("test")
     public void testTransactional(){
         cService.testTransactional();
+    }
+
+    @GetMapping("testLock")
+    public void testLock() throws InterruptedException {
+        RLock lock = redisson.getLock("lock");
+        boolean res = lock.tryLock(100, 10, TimeUnit.SECONDS);
+
+        lock.unlock();
+
     }
 }
