71f32445cd10fc9c81ba319a1bdb001e3daec7f3
hongyuqin <896664230@qq.com>
2025-04-18 10:15:36 +0800

    redisson 看看限流

--- src/main/java/com/huf/g1test/controller/HelloController.java
+++ src/main/java/com/huf/g1test/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.huf.g1test.controller;
 import com.huf.g1test.service.CService;
 import org.redisson.Redisson;
 import org.redisson.api.RLock;
+import org.redisson.api.RRateLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,9 @@ public class HelloController {
     @GetMapping("testLock")
     public void testLock() throws InterruptedException {
         RLock lock = redisson.getLock("lock");
+        //看看怎么限流
+        RRateLimiter rateLimiter = redisson.getRateLimiter("rate:limit");
+        rateLimiter.acquire();
         boolean res = lock.tryLock(100, 10, TimeUnit.SECONDS);
 
         lock.unlock();
